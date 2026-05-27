15d04358c4684d95ce2f05b86878eaa31a164e6f
Hamed Ghasemzadeh <ghasemzadeh.it@gmail.com>
2026-05-27 13:23:08 +0330

    Delete app directory

--- app/src/main/java/com/example/productgallery/ui/navigation/AppDestination.kt
+++
@@ -1,7 +0,0 @@
-package com.example.productgallery.ui.navigation
-
-enum class AppDestination(val route: String, val label: String) {
-    Gallery("gallery", "Gallery"),
-    Requests("requests", "Requests"),
-    Import("import", "Import")
-}
